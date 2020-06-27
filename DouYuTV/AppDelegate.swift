2220c53c26b196a94405a1449caef54db532d6dc
huangwu1111 <1243930473@qq.com>
2020-06-27 11:22:01 +0800

    huhu

---
+++ DouYuTV/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  DouYuTV
+//
+//  Created by 黄武 on 2020/6/26.
+//  Copyright © 2020 黄武. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+//        self.window?.rootViewController = ViewController.init()
+//        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+   
+
+
+}
+
---
+++ DouYuTV/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DouYuTV
+//
+//  Created by 黄武 on 2020/6/26.
+//  Copyright © 2020 黄武. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red;
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
